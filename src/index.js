c244c779508faebd8f239d5d5bedddf44060fa3c
Will D <pleplu01@gmail.com>
2023-07-27 18:22:55 -0400

    Added Atatus to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('aee318c36d4c41838d4f83a1d334f435').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
