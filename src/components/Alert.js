9798658de30aba71d0f038a28ef17293da08fb58
Will D <pleplu01@gmail.com>
2023-07-31 19:34:27 -0400

    updated error color

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -45,8 +45,8 @@ export { InfoAlert };
 class ErrorAlert extends Alert {
     constructor(props) {
         super(props);
-        this.color = ''; //red 
-        this.bgColor = ''; //light red
+        this.color = 'rgb(255, 0, 0)'; //red 
+        this.bgColor = 'rgb(255, 220, 220)'; //light red
     }
 }
 
